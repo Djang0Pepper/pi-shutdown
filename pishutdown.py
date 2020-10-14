20d3f00e8bb91993a52323efe36f31238fd247ee
fr3d-mobile <fr3d-mobile@bitbucket.org>
2020-10-15 00:15:52 +0200

    maj du service,  et ajout d'une led

--- pishutdown.py
+++ pishutdown.py
@@ -1,5 +1,18 @@
+
+
 #!/usr/bin/python
 # shutdown/reboot(/power on) Raspberry Pi with pushbutton
+
+#pi-shutdown
+#===========
+#Shutdown/reboot(/power on) Raspberry Pi with pushbutton
+# Usage:
+#Connect pushbutton to GPIO pin 5 and ground then run:
+#```
+#sudo python pishutdown.py
+#```
+#When button is pressed for less than 3 seconds, Pi reboots. If pressed for more than 3 seconds it shuts down.
+#While shut down, if button is connected to GPIO pin 5, then pressing the button powers on Pi.
 
 import RPi.GPIO as GPIO
 from subprocess import call
@@ -8,7 +21,9 @@ import time
 
 # pushbutton connected to this GPIO pin, using pin 5 also has the benefit of
 # waking / powering up Raspberry Pi when button is pressed
-shutdownPin = 401
+shutdownPin = 21
+LED = 26
+
 
 # if button pressed for at least this long then shut down. if less then reboot.
 shutdownMinSeconds = 3
@@ -16,11 +31,13 @@ shutdownMinSeconds = 3
 # button debounce time in seconds
 debounceSeconds = 0.01
 
-GPIO.setmode(GPIO.BOARD)
+#GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(shutdownPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(LED, GPIO.OUT)
 
 buttonPressedTime = None
-
 
 def buttonStateChanged(pin):
     global buttonPressedTime
@@ -37,9 +54,11 @@ def buttonStateChanged(pin):
             if elapsed >= shutdownMinSeconds:
                 # button pressed for more than specified time, shutdown
                 call(['shutdown', '-h', 'now'], shell=False)
+                GPIO.output(LED, GPIO.LOW) #On l’éteint
             elif elapsed >= debounceSeconds:
                 # button pressed for a shorter time, reboot
                 call(['shutdown', '-r', 'now'], shell=False)
+                GPIO.output(LED, GPIO.HIGH) #On l’allume
 
 
 # subscribe to button presses
@@ -47,4 +66,7 @@ GPIO.add_event_detect(shutdownPin, GPIO.BOTH, callback=buttonStateChanged)
 
 while True:
     # sleep to reduce unnecessary CPU usage
-    time.sleep(5)
+    GPIO.output(LED, GPIO.LOW) #On l’éteint
+    time.sleep(3)
+    GPIO.output(LED, GPIO.HIGH) #On l'allume
+    time.sleep(2)
